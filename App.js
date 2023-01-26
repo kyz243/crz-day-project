8bef99aadb5f52330c953e27fc72f125cda56803
kyz243 <66202551+kyz243@users.noreply.github.com>
2023-01-27 00:33:52 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Try e 🎉
+      </Text>
+    </View>
+  );
+}
+
+export default App;
